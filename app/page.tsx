fd1bfa9954a7a2575588abd2ad5f9bee3faab15f
yusufhamed <Yusuf.hamed04@gmail.com>
2024-01-03 16:32:29 -0500

    removed unessecary test features in nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <main className="">
-      <h1>Lets build disney</h1>
-      <Button>Click me</Button>
     </main>
   )
 }
