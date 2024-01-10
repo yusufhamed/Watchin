5892d77862d5a7fe6440ef690e300b6f36ac77cd
Pavithiran <pavinagul29@gmail.com>
2024-01-10 16:20:43 -0500

    added both genre and search functionality

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,3 +1,6 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
+
 type Props = {
     params: {
         id: string
@@ -7,12 +10,19 @@ type Props = {
     }
 };
 
-function GenrePage({params: {id}, searchParams: {genre}}: Props) {
+async function GenrePage({params: {id}, searchParams: {genre}}: Props) {
+  const movies = await getDiscoverMovies(id);
+
+
+
   return (
-    <div>
-      Welcome to the genre page for {genre}! and ID of {id}
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+        <MoviesCarousel title={`Genre`} movies={movies} isVertical/>
+      </div>
     </div>
-  )
+  );
 }
 
 export default GenrePage
