58f6765dd3bb7741229cd876cb926584fc933be5
tbaboolal <tyler.baboolal09@gmail.com>
2024-01-26 13:03:31 -0500

    added styling for trending page, also added the ability for pagination, reworked logic of Trending page as well to allow for other categories in the future

--- app/trending/page.tsx
+++ app/trending/page.tsx
@@ -1,12 +1,50 @@
 import Trending from '../../components/trending'
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+  } from "@/components/ui/pagination"
 
-function TrendingPage(){
+function TrendingPage({searchParams}: {searchParams: {page: string | "undefined"}}){    
+    let page: string
+    if (searchParams.page === undefined || isNaN(parseInt(searchParams.page))|| searchParams.page=='0'){
+         page = '0';
+    }else{
+         page = searchParams.page;
+    }
+    const previousPage = parseInt(page) - 1;
+    const nextPage=parseInt(page)+1;
+
+    
 
     return(
         <>
-            <h1>Movies: </h1>
-            <h2>Trending</h2>
-            <Trending />
+            <h1  className="text-6xl font-bold px-10">Trending </h1>
+            <h2 className="text-4xl font-bold px-10 ">Movies</h2>
+            <Trending {...searchParams} />
+            <Pagination>
+                <PaginationContent>
+                    {page!=='0' &&(
+                        <PaginationItem>
+                            <PaginationPrevious href={`/trending?page=${previousPage}`}/> 
+                        </PaginationItem>
+                        )
+                    }
+                    <PaginationItem>
+                        <PaginationLink href="#" isActive>{page}</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href={`/trending?page=${nextPage}`}  />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </>
     ); 
 }
