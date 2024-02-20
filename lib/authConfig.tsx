ad10603141c36fc1412ec9213399508c39148980
yusufhamed <Yusuf.hamed04@gmail.com>
2024-02-20 12:57:56 -0500

    changed secret

--- lib/authConfig.tsx
+++ lib/authConfig.tsx
@@ -4,7 +4,7 @@ import TraktProvider from 'next-auth/providers/trakt';
 import GoogleProvider from "next-auth/providers/google";
 
 export const authConfig: NextAuthOptions ={
-    secret: process.env.NEXT_PUBLIC_SECRET,
+    secret: process.env.SECRET,
     providers:[
         TraktProvider({
             clientId:process.env.TRAKT_CLIENT_ID as string,
